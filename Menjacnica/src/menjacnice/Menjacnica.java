4925af7b29d2fc8fafa920fa77a4feeb6d6f2713
jovanmilovanovic721 <mr.jovan94@hotmail.com>
2015-04-02 18:47:12 +0200

    Implementirane metode iz interfejsa; nefunkcionalne.

---
+++ Menjacnica/src/menjacnice/Menjacnica.java
@@ -0,0 +1,28 @@
+package menjacnice;
+
+import java.util.GregorianCalendar;
+
+import kursevi.Kursevi;
+import interfejsmenjacnica.InterfejsMenjacnica;
+
+public class Menjacnica implements InterfejsMenjacnica{
+
+	public void dodajKurs(GregorianCalendar datum, double prodajni,
+			double srednji, double kupovni) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void obrisiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Kursevi pronadjiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
