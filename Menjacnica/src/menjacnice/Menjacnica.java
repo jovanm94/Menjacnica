a59e830784575975cb431f37ebe891dc5f33da72
jovanmilovanovic721 <mr.jovan94@hotmail.com>
2015-04-02 19:29:08 +0200

    ispravka kod metoda

--- Menjacnica/src/menjacnice/Menjacnica.java
+++ Menjacnica/src/menjacnice/Menjacnica.java
@@ -1,27 +1,33 @@
 package menjacnice;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
+import valute.Valute;
 import kursevi.Kursevi;
 import interfejsmenjacnica.InterfejsMenjacnica;
 
 public class Menjacnica implements InterfejsMenjacnica{
 
-	public void dodajKurs(GregorianCalendar datum, double prodajni,
+	private LinkedList<Valute> valute;
+	
+	public void dodajKurs(Valute valute, GregorianCalendar datum, double prodajni,
 			double srednji, double kupovni) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void obrisiKurs(GregorianCalendar datum) {
+	public void obrisiKurs(Valute valute, GregorianCalendar datum) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public Kursevi pronadjiKurs(GregorianCalendar datum) {
+	public Kursevi pronadjiKurs(Valute valute, GregorianCalendar datum) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 
 	
 	
