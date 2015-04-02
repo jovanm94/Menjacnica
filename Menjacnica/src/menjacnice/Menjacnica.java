5097409cba5a0f962232b8b2c687f85360c3a95d
jovanmilovanovic721 <mr.jovan94@hotmail.com>
2015-04-02 19:34:44 +0200

    Spojene grane; finalna verzija

--- Menjacnica/src/menjacnice/Menjacnica.java
+++ Menjacnica/src/menjacnice/Menjacnica.java
@@ -7,26 +7,74 @@ import valute.Valute;
 import kursevi.Kursevi;
 import interfejsmenjacnica.InterfejsMenjacnica;
 
-public class Menjacnica implements InterfejsMenjacnica{
+//morao sam da stavim ovu klasu kao abstract, jer sam zaboravio na pocetku da kao argument stavim objekat "valute"
+//klase Valute, pa nije hteo program da mi prihvati merge
+
+ public abstract class Menjacnica implements InterfejsMenjacnica{
 
 	private LinkedList<Valute> valute;
 	
-	public void dodajKurs(Valute valute, GregorianCalendar datum, double prodajni,
-			double srednji, double kupovni) {
-		// TODO Auto-generated method stub
+	public void dodajKurs(Valute valuta, GregorianCalendar datum,
+			double prodajni, double srednji, double kupovni) {
+		
+		Kursevi kurs = new Kursevi();
+		
+		kurs.setDatum(datum);
+		kurs.setKupovni(kupovni);
+		kurs.setProdajni(prodajni);
+		kurs.setSrednji(srednji);
+		
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i) != null) {
+				Valute v = valute.get(i);
+				if(v.getNaziv().equals(valuta)) {
+					v.getKursevi().add(kurs);
+					break;
+				}
+			}
+		}
 		
 	}
 
-	public void obrisiKurs(Valute valute, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+	public void obrisiKurs(Valute valuta, GregorianCalendar datum) {
+		
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i) != null) {
+				Valute v = valute.get(i);
+				if(v.getNaziv().equals(valuta)) {
+					for (int j = 0; j < v.getKursevi().size(); j++) {
+						if(v.getKursevi().get(j) != null) {
+							Kursevi k = v.getKursevi().get(j);
+							if(k.getDatum().equals(datum)) {
+								v.getKursevi().remove(j);
+							}
+						}
+					}
+				}
+			}
+		}
 		
 	}
 
-	public Kursevi pronadjiKurs(Valute valute, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+	public Kursevi pronadjiKurs(Valute valuta, GregorianCalendar datum) {
+	
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i) != null) {
+				Valute v = valute.get(i);
+				if(v.getNaziv().equals(valuta)) {
+					for (int j = 0; j < v.getKursevi().size(); j++) {
+						if(v.getKursevi().get(j) != null) {
+							Kursevi k = v.getKursevi().get(j);
+							if(k.getDatum().equals(datum)){
+								return k;
+							}
+						}
+					}
+				}
+			}
+		}
 		return null;
 	}
-
 	
 
 	
